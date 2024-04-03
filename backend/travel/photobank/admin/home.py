1c29e1d2d57f635f6f5e799afb25ded7bfc485ba
sssemion <ssemi.on@yandex.ru>
2024-04-04 02:05:31 +0300

    home summary item

--- backend/travel/photobank/admin/home.py
+++ backend/travel/photobank/admin/home.py
@@ -1,6 +1,29 @@
+from wagtail.admin.navigation import get_site_for_user
+from wagtail.admin.site_summary import SiteSummaryPanel
 from wagtail.admin.views.home import HomeView
+from wagtail.images.wagtail_hooks import SummaryItem
+
+from photobank.models import Photo
 
 
 class PhotobankHomeView(HomeView):
     def get_panels(self):
-        return []  # TODO PhotosPanel
+        return [Dashboard(self.request)]  # TODO PhotosPanel
+
+
+class Dashboard(SiteSummaryPanel):
+    def __init__(self, request):
+        self.request = request
+        self.summary_items = [PhotosSummary(self.request)]
+
+
+class PhotosSummary(SummaryItem):
+    template_name = 'photos_summary_item.html'
+
+    def get_context_data(self, parent_context):
+        site_name = get_site_for_user(self.request.user)["site_name"]
+
+        return {
+            "total_images": Photo.objects.filter(hidden=False).count(),
+            "site_name": site_name,
+        }
